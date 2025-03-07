07970da3a18dba71642c60ae380fe0a1ed7d1650
ZacharyWoolever <zachary.woolever@amount.com>
2025-03-07 10:27:53 -0600

    removing unused POC

--- apps/product-engine-be/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-declare const __BUILD_VERSION__: string;
-
-export const environment = {
-  production: false,
-  version: __BUILD_VERSION__,
-  platformRepoPath:
-    '/Users/ZacharyWoolever/Documents/git/ODXP-DPLOY--odx-config-platform-deploy',
-};
